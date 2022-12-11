0514be6d1b516ef5b0c8ecf5fdc3ac793c7ade5e
salma yahyaoui <yahyaouisalma14@gmail.com>
2022-12-12 00:43:59 +0100

    design

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit { 
-  placeholdermail="myUserEmail"
-  constructor() { }
+export class LoginComponent implements OnInit {
+  placeholdermail = 'myUserEmail';
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
-  sendForm(form:NgForm){
-    console.log("eee",form.formDirective);
-    
+  ngOnInit(): void {}
+  sendForm(form: NgForm) {
+    console.log('eee', form.formDirective);
+    this.router.navigate(['cv']);
   }
 }
